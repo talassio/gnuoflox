a4a744c51a6ba865a4069b66c1e62b5dadb5a4ce
talassio@gmail.com <talassio@gmail.com@f2dad118-a0a9-11de-bf0a-f72303de72c1>
2009-11-07 01:09:18 +0000

    Added a packaging tool.

---
+++ trunk/tools/mkpkg.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo -n 'Version number to use: (example 1.0.5) '
+read VERSION
+echo tar czvf gnuoflox-${VERSION}.tgz --exclude=.svn ./gnuoflox/inst ./gnuoflox/doc ./gnuoflox/DESCRIPTION ./gnuoflox/INDEX ./gnuoflox/COPYING
